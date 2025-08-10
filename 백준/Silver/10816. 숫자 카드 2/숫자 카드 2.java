24b9bc28e6bb79c307e498cbf89117ecaa990695
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-08-11 03:16:18 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1184 ms, Memory: 184632 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, Integer> hashMap = new HashMap();
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (hashMap.containsKey(num)) {
+                hashMap.put(num, hashMap.get(num) + 1);
+            } else {
+                hashMap.put(num, 1);
+            }
+        }
+        st = new StringTokenizer(bf.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if (hashMap.get(target) == null) {
+                sb.append(0).append(" "); 
+            }else {
+                sb.append(hashMap.get(target)).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
