3dd0a3775e9ff158207e665ad57949d1cb0b4c72
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-08-24 03:43:53 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,42 +3,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
+
     static int[] arr;
+    static final int INF = 1000000;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        
+
         int N = Integer.parseInt(st.nextToken());
 
-        arr = new int[N+1];
+        arr = new int[N + 1];
 
         recur(N);
 
         System.out.println(arr[N]);
     }
 
-    static void recur(int num) {
-        if(num <= 0) return;
-        if(num <= 3 && num > 1) {
-            arr[num] = 1;
-            return;
-        } else if(num == 1) {
-            arr[num] = 0;
-            return;
+    static int recur(int num) {
+        if (num == 1) {
+            return 0;
         }
-        if(arr[num] == 0) {
-            recur(num/3);
-            recur(num/2);
-            recur(num-1);
-
-            int min = Integer.MAX_VALUE;
-
-            if(num/3 != 0 && num % 3 == 0) min = Math.min(arr[num/3], min);
-            if(num/2 != 0 && num % 2 == 0) min = Math.min(arr[num/2], min);
-            if(num-1 != 0) min = Math.min(arr[num-1], min);
-            
-            arr[num] = min + 1;
+        if (arr[num] != 0) {
+            return arr[num];
         }
+
+        int min = INF;
+
+        if (num % 3 == 0) {
+            min = Math.min(min, recur(num/3));
+        }
+        if (num % 2 == 0) {
+            min = Math.min(min, recur(num/2));
+        }
+        min = Math.min(min, recur(num-1));
+
+        return arr[num] = min+1;
 
     }
 }
