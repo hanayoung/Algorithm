0cbde9e29c593e465be7aabde9ad522da1488bd7
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-09-25 18:43:06 +0900

    [Silver I] Title: 안전 영역, Time: 304 ms, Memory: 59336 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -6,69 +6,62 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
-  static int[][] cloned;
-  static int N;
-  static int[][] maps;
-  static int[] dx = {-1,1,0,0};
-  static int[] dy = {0,0,-1,1};
-  static int maxCnt = 0;
-  public static void main(String[] args) throws Exception{
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(bf.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-    N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-    maps = new int[N][N];
-    int max = 0;
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(bf.readLine());
-      for (int j = 0; j < N; j++) {
-        maps[i][j] = Integer.parseInt(st.nextToken());
-        if(max < maps[i][j]) max = maps[i][j];
-      }        
-    }
-    for (int i = 0; i < max; i++) {
-        makeClone(i);
-        maxCnt = Math.max(findArea(), maxCnt);
-    }
+        int[][] maps = new int[N][N];
+ 
+        int maxHeight = 0; // 가장 높은 지역의 높이
+        int minHeight = 100;
+        int answer = 1;
 
-    System.out.println(maxCnt);
-  }
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,1,-1};
 
-  static void makeClone(int height){
-    cloned = new int[N][N];
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        cloned[i][j] = maps[i][j] - height;
-      }
-    }
-  }
-
-  static int findArea() {
-    boolean[][] visited = new boolean[N][N];
-    int area = 0;
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if(cloned[i][j] > 0 && visited[i][j] == false){
-          Queue<Point> queue = new LinkedList<>();
-          queue.add(new Point(i,j));
-          area += 1;
-          while(queue.size() != 0){
-            Point p = queue.poll();
-            for (int k = 0; k < 4; k++) {
-              int nx = p.x + dx[k];
-              int ny = p.y + dy[k];
-              if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-
-              if(cloned[nx][ny] > 0 && visited[nx][ny] == false) {
-                visited[nx][ny] = true;
-                queue.add(new Point(nx,ny));
-              }
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for(int j = 0; j < N; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
+                maxHeight = Math.max(maxHeight, maps[i][j]);
+                minHeight = Math.min(minHeight, maps[i][j]);
             }
-          }
         }
-      }
+
+        for(int height = minHeight; height < maxHeight; height++) {
+            boolean[][] visited = new boolean[N][N];
+            int area = 0;
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(maps[i][j] - height > 0 && visited[i][j] == false) {
+                        Queue<Point> q = new LinkedList<>();
+                        q.add(new Point(i,j));
+                        visited[i][j] = true;
+                        area++;
+                        while(q.size() > 0) {
+                            Point cur = q.poll();
+
+                            for(int l = 0; l < 4; l++) {
+                                int nx = cur.x + dx[l];
+                                int ny = cur.y + dy[l];
+
+                                if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+                                if(visited[nx][ny] == false && maps[nx][ny] - height > 0) {
+                                    q.add(new Point(nx, ny));
+                                    visited[nx][ny] = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            answer = Math.max(area, answer);
+        }
+        System.out.println(answer);
+
+        
     }
-    return area;
-  }
 }
