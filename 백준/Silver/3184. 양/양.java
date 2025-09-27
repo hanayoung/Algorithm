92a8284aaf5fa22472105d3d2383ee2b6d7f3b3c
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-09-27 16:32:10 +0900

    [Silver I] Title: 양, Time: 184 ms, Memory: 20372 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.java
+++ 백준/Silver/3184. 양/양.java
@@ -1,4 +1,3 @@
-
 import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,66 +6,67 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
-  public static void main(String[] args) throws Exception{
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(bf.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-    int R = Integer.parseInt(st.nextToken());
-    int C = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-    int[] dx = {-1,1,0,0};
-    int[] dy = {0,0,-1,1};
+        char[][] maps = new char[R][C];
+        boolean[][] visited = new boolean[R][C];
 
-    Character[][] maps = new Character[R][C];
-    boolean[][] visited = new boolean[R][C];
+        int[] answer = new int[2];
 
-    int wolfCnt = 0;
-    int sheepCnt = 0;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
 
-    for(int i = 0; i < R; i++){
-      String str = new StringTokenizer(bf.readLine()).nextToken();
-      for (int j = 0; j < C; j++) {
-        maps[i][j] = str.charAt(j);
-      }
-    }
-
-    for (int i = 0; i < R; i++) {
-      for (int j = 0; j < C; j++) {
-        if(visited[i][j] == false && maps[i][j] != '#'){
-          int wCnt = 0;
-          int sCnt = 0;
-          switch(maps[i][j]){
-            case 'o' : sCnt += 1;
-            break;
-            case 'v' : wCnt += 1;
-            break;
-          }
-          visited[i][j] = true;
-          Queue<Point> queue = new LinkedList<>();
-          queue.add(new Point(i,j));
-
-          while(queue.size() != 0) {
-            Point p = queue.poll();
-            for (int k = 0; k < 4; k++) {
-              int nx = p.x + dx[k];
-              int ny = p.y + dy[k];
-
-              if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-
-              if(visited[nx][ny] == false && maps[nx][ny] != '#'){
-                visited[nx][ny] = true;
-                queue.add(new Point(nx,ny));
-                if(maps[nx][ny] == 'o') sCnt += 1;
-                else if(maps[nx][ny] == 'v') wCnt += 1;
-              }
+        for(int i = 0; i < R; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String str = st.nextToken();
+            for(int j = 0; j < C; j++) {
+                maps[i][j] = str.charAt(j);
             }
-          }
-          if(sCnt > wCnt) sheepCnt += sCnt;
-          else wolfCnt += wCnt;
         }
-      }
-    }
 
-    System.out.println(sheepCnt+" "+wolfCnt);
-  }
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                if(visited[i][j] == false && (maps[i][j] == 'v' || maps[i][j] == 'o')) {
+                    int sheep = 0;
+                    int wolf = 0;
+
+                    Queue<Point> q = new LinkedList<>();
+                    q.add(new Point(i,j));
+                    visited[i][j] = true;
+
+                    while(q.size() > 0) {
+                        Point cur = q.poll();
+                        switch(maps[cur.x][cur.y]) {
+                            case 'v': wolf++;
+                            break;
+                            case 'o': sheep++;
+                        }
+                        
+                        for(int l = 0; l < 4; l++) {
+                            int nx = cur.x + dx[l];
+                            int ny = cur.y + dy[l];
+
+                            if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+
+                            if(visited[nx][ny] == false && maps[nx][ny] != '#') {
+                                visited[nx][ny] = true;
+                                q.add(new Point(nx, ny));
+                            }
+                        }
+                    }
+                    if(sheep <= wolf) sheep = 0;
+                    else wolf = 0;
+
+                    answer[0] += sheep;
+                    answer[1] += wolf;
+                }
+            }
+        }
+        System.out.println(answer[0]+" "+answer[1]);
+    }
 }
