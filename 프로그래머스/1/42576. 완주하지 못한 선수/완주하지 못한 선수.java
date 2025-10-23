fbac19ea31e285b9b60832ffbf71388a8d736fbd
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-10-24 03:50:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.54 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Map<String, Integer> playerMap = new HashMap<>();
+        for(String player: participant) {
+            playerMap.put(player, playerMap.getOrDefault(player, 0) +1);
+        }
+
+        for(String player: completion) {
+            if(playerMap.get(player) - 1 == 0) {
+                playerMap.remove(player);
+            } else playerMap.put(player, playerMap.get(player) - 1);
+        }
+
+        for(Map.Entry<String, Integer> entry: playerMap.entrySet()) {
+            answer = entry.getKey();
+        }
+        return answer;
+    }
+}
