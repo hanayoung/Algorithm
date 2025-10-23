1e6f28225237272ad43bff02084d5a8dae313fd0
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-10-24 04:31:57 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 87.9 MB, 시간: 0.10 ms
+메모리: 87.3 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 16일 21:50:31
+2025년 10월 24일 04:31:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,17 @@
-
+import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String, Integer> sets = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            if(sets.containsKey(clothes[i][1])) {
-                sets.put(clothes[i][1], sets.get(clothes[i][1]) + 1);
-            }else{
-                sets.put(clothes[i][1], 1);
-            }
+        Map<String, Integer> clothesMap = new HashMap<>();
+        for(String[] cloth: clothes) {
+            clothesMap.put(cloth[1], clothesMap.getOrDefault(cloth[1], 0)+1);
         }
-        for (Integer val : sets.values()) {
-            answer = answer * (val+1);
+        for(Integer size: clothesMap.values()) {
+            answer *= (size + 1);
         }
         return answer-1;
     }
