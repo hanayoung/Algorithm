1a8f7aea35de73d32a93385237ff2309d0ccb973
aoreo <81007133+hanayoung@users.noreply.github.com>
2025-10-30 21:39:33 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.13 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -17,7 +17,6 @@ class Solution {
             tree.computeIfAbsent(wire[1], k -> new ArrayList<Integer>()).add(wire[0]);
         }
         for(int[] wire: wires) {
-            // wire을 끊기 [1,3]을 끊어버리면?
             Map<Integer, Boolean> set1 = new HashMap<>();
             Map<Integer, Boolean> set2 = new HashMap<>();    
             set1.put(wire[0], true);
@@ -49,29 +48,9 @@ class Solution {
                     }
                 }
             }
-            
             answer = Math.min(answer, Math.abs(set2.size() - set1.size()));
-            if(answer == 0) System.out.println(wire[0]+" "+wire[1]+" "+set1.size()+" "+set2.size());
-        }
-//         for(Map.Entry<Integer, List<Integer> entry: tree.entrySet()) {
-//             int cnt = 0;
-//             int key = entry.getKey();
-//             List<Integer> values = entry.getValue();
-//             if(values != null) {
-//                 Queue<Integer> q = new LinkedList<>();
-//                 q.addAll(values);
-//                 cnt = values.size();
-//                 while(q.size() > 0) {
-//                     int node = q.poll();
-                    
-//                 }
-//             }
-
-//         }
-        
-        // 노드 하나씩 선택해서 연결된 개수 파악
-        // 각 노드에 연결된 것들도 넣되, 어딘가에다가 연결된 노드 개수 저장해둬야 계속 세는 걸 방지
-        
+            if(answer == 0) break;
+        }        
         return answer;
     }
 }
