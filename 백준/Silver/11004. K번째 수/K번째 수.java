7c25f12350de09de6e607490a6e053c3401427bf
aoreo <81007133+hanayoung@users.noreply.github.com>
2026-03-22 21:41:38 +0900

    [Silver V] Title: K번째 수, Time: 2504 ms, Memory: 458876 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -9,19 +9,16 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         st = new StringTokenizer(bf.readLine());
 
         for(int i = 0; i < N; i++) {
             pq.add(Integer.valueOf(st.nextToken()));
+            if(pq.size() > K) {
+                pq.poll();
+            }
         }
 
-        int idx = 1;
-        while(idx < K) {
-            pq.poll();
-            idx++;
-        }
-
-        System.out.println(pq.poll());
+        System.out.println(pq.peek());
     }
 }
